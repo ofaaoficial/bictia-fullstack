28b49fe085c61c17e785830c1267aca505ae16aa
Desarrollador 2 <=>
2020-01-29 18:22:47 -0500

    taller 29 01 2019 filtro

---
+++ ejercicios/clase-7 29 01 2020/filtroCategoria/assets/js/app.js
@@ -0,0 +1,85 @@
+const productos = [
+    {
+        nombre: "Celular 1",
+        precio: "400000",
+        imagen: "assets/imgs/celular.jpg",
+        categoria: "tecnologia"
+    },
+    {
+        nombre: "Equipo 1",
+        precio: "1200000",
+        imagen: "assets/imgs/pc.jpg",
+        categoria: "tecnologia"
+    },
+    {
+        nombre: "Saco 1",
+        precio: "50000",
+        imagen: "assets/imgs/saco.jpg",
+        categoria: "ropa"
+    },
+    {
+        nombre: "Pantalon 1",
+        precio: "50000",
+        imagen: "assets/imgs/pantalon.jpg",
+        categoria: "ropa"
+    },
+    {
+        nombre: "Celular 2",
+        precio: "400000",
+        imagen: "assets/imgs/celular.jpg",
+        categoria: "tecnologia"
+    },
+    {
+        nombre: "Equipo 2",
+        precio: "1400000",
+        imagen: "assets/imgs/pc.jpg",
+        categoria: "tecnologia"
+    },
+];
+
+const renderizarContenido = (productos) => {
+    let html = '';
+    productos.forEach(producto => html += generarElemento(producto));
+    document.querySelector('#container').innerHTML = html;
+};
+
+const generarElemento = (producto) => {
+    return `<article class="producto" data-category="${producto.categoria}">
+                <h2>${producto.nombre}</h2>
+                <img src="${producto.imagen}" alt="img">
+                <span class="precio">$ ${producto.precio}</span>
+            </article>`;
+};
+
+renderizarContenido(productos);
+
+document.querySelector('#filtro').addEventListener('change', function (event) {
+    event.preventDefault();
+
+    let filtro = this.value;
+
+    if (filtro !== 'todo') {
+        document.querySelectorAll(`.producto:not([data-category="${filtro}"])`)
+            .forEach(function (element) {
+                element.style.display = 'none';
+            });
+
+        document.querySelectorAll(`.producto[data-category="${filtro}"]`)
+            .forEach(function (element) {
+                element.style.display = 'flex';
+            });
+    } else {
+        document.querySelectorAll(`.producto`)
+            .forEach(function (element) {
+                element.style.display = 'flex';
+            });
+    }
+
+});
+
+document.querySelector('#ordernar').addEventListener('change', function (event) {
+    event.preventDefault();
+    let filtro = this.value;
+    if(filtro !== 'normal') renderizarContenido(productos.sort((a, b) => a[filtro] > b[filtro] ? 1 : -1));
+    else renderizarContenido(productos);
+});
---
+++ ejercicios/clase-7 29 01 2020/objetos.js
@@ -0,0 +1,8 @@
+/**
+ * Mapas en otro lenguajes
+ *  [clave => valor]
+ *
+ *  Crear un arreglo de objetos para renderizarlos en una vista.
+ *  Ordenarlos con una lista utilizar sort()
+ */
+
